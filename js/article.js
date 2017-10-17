d8d58ba082f2b13da9aa3d45187378a36b202e00
Patrick Weston <patrickfweston@gmail.com>
2017-10-16 21:47:44 -0400

    Automatic content deploy (2017-10-16 21:47:44)

--- js/article.js
+++ js/article.js
@@ -8,3 +8,23 @@
     });
   });
 })();
+
+// From: https://css-tricks.com/reading-position-indicator/
+(function() {
+  $(document).on('ready', function() {
+    var winHeight = $(window).height(),
+        docHeight = $(document).height(),
+        progressBar = $('.progress'),
+        max, value, barWidth, percentageViewed;
+
+    /* Set the max scrollable area */
+    max = (docHeight - winHeight) / 100;
+
+    $(document).on('scroll', function(){
+       value = $(window).scrollTop();
+       percentageViewed = value / max;
+       barWidth = percentageViewed > 100 ? 100 : percentageViewed;
+       progressBar.css('width', barWidth + '%');
+    });
+  });
+})();
